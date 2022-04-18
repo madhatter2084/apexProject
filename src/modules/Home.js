173a6729877a047a730ef2e10e1eea2c46d6f457
madhatter <matthew.rikard@gmail.com>
2022-04-18 16:01:28 -0400

    background image cleaned up

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home">What's the home page gonna be?????</div>;
+  return (
+    <div className="home">
+      <div className="home__tile">
+        <div className="home__button_grp"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
