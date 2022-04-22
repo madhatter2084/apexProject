a16aba395ab4a2a73205504640ce1e5392dfaadb
madhatter <matthew.rikard@gmail.com>
2022-04-22 11:00:39 -0400

    added onClick to nav links. nav menu will automatically close when a link is clicked.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,6 +9,11 @@ function Nav() {
     nav.classList.toggle("nav__expanded");
   }
 
+  function closeNav() {
+    let nav = document.getElementById("navUl");
+    nav.classList.remove("nav__expanded");
+  }
+
   return (
     <div className="nav">
       <img
@@ -19,33 +24,33 @@ function Nav() {
       />
       <ul className="nav__links" id="navUl">
         <li>
-          <Link to="/" className="nav__link">
+          <Link to="/" className="nav__link" onClick={closeNav}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="/stats" className="nav__link">
+          <Link to="/stats" className="nav__link" onClick={closeNav}>
             Stats
           </Link>
         </li>
         <li>
-          <Link to="/map" className="nav__link">
+          <Link to="/map" className="nav__link" onClick={closeNav}>
             Map
           </Link>
         </li>
         <li>
-          <Link to="/news" className="nav__link">
+          <Link to="/news" className="nav__link" onClick={closeNav}>
             News
           </Link>
         </li>
 
         <li>
-          <Link to="/replicator" className="nav__link">
+          <Link to="/replicator" className="nav__link" onClick={closeNav}>
             Replicator
           </Link>
         </li>
         <li>
-          <Link to="/shop" className="nav__link">
+          <Link to="/shop" className="nav__link" onClick={closeNav}>
             Shop
           </Link>
         </li>
