ad387d74e9d95f00465a2bbd2ae6b5b053c52ff3
madhatter <matthew.rikard@gmail.com>
2022-04-27 19:12:43 -0400

    radio, text input, and search button added to stats page

--- src/modules/Stats.js
+++ src/modules/Stats.js
@@ -4,7 +4,26 @@ import "./Stats.css";
 function Character() {
   return (
     <div className="stats">
-      <input type="text" placeholder="IxMADHATTERxI" id="stats__text"></input>
+      <div className="stats__box">
+        <div className="stats__platform">
+          <input type="radio" id="pc" name="platform" value="PC" />
+          <label for="pc">PC</label>
+          <input type="radio" id="ps4" name="platform" value="PS4" />
+          <label for="ps4">Playstation</label>
+          <input type="radio" id="xbox" name="platform" value="X1" />
+          <label for="xbox">XBox</label>
+        </div>
+        <div className="stats__gamerTag">
+          <input
+            type="text"
+            placeholder="Gamertag/PSN ID/Steam ID"
+            id="stats__text"
+          />
+        </div>
+        <button type="submit" className="stats__btn">
+          Search
+        </button>
+      </div>
     </div>
   );
 }
