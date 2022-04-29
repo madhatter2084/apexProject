2f3c430b8fc72c380f10189825ba50aa0fe057c1
madhatter <matthew.rikard@gmail.com>
2022-04-29 10:54:31 -0400

    styles added for Error.js

--- src/modules/Error.js
+++ src/modules/Error.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Error.css";
 
 function Error() {
-  return <div>Error! Page not found =(</div>;
+  return (
+    <div className="error">
+      <h1 className="error__message">Error! Page not found.</h1>
+    </div>
+  );
 }
 
 export default Error;
--- src/modules/Stats.js
+++ src/modules/Stats.js
@@ -5,24 +5,26 @@ function Character() {
   return (
     <div className="stats">
       <div className="stats__box">
-        <div className="stats__platform">
-          <input type="radio" id="pc" name="platform" value="PC" />
-          <label for="pc">PC</label>
-          <input type="radio" id="ps4" name="platform" value="PS4" />
-          <label for="ps4">Playstation</label>
-          <input type="radio" id="xbox" name="platform" value="X1" />
-          <label for="xbox">XBox</label>
-        </div>
-        <div className="stats__gamerTag">
-          <input
-            type="text"
-            placeholder="Gamertag/PSN ID/Steam ID"
-            id="stats__text"
-          />
-        </div>
-        <button type="submit" className="stats__btn">
-          Search
-        </button>
+        <form autocomplete="off" name="platform">
+          <div className="stats__platform">
+            <input type="radio" id="pc" name="platform" value="PC" />
+            <label for="pc">PC</label>
+            <input type="radio" id="ps4" name="platform" value="PS4" />
+            <label for="ps4">Playstation</label>
+            <input type="radio" id="xbox" name="platform" value="X1" />
+            <label for="xbox">XBox</label>
+          </div>
+          <div className="stats__gamerTag">
+            <input
+              type="text"
+              placeholder="Gamertag/PSN ID/Steam ID"
+              id="stats__text"
+            />
+          </div>
+          <button type="submit" className="stats__btn">
+            Search
+          </button>
+        </form>
       </div>
     </div>
   );
