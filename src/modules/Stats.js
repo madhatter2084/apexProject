514fdf1d4f12f7ed6977988b9fc8cfc36988d56c
madhatter <matthew.rikard@gmail.com>
2022-05-20 19:08:10 -0400

    categories added to stats.js

--- src/modules/Stats.js
+++ src/modules/Stats.js
@@ -62,24 +62,34 @@ function Character() {
           <div className="stats__gamerTag">
             <input
               type="text"
-              placeholder="Gamertag/PSN ID/Steam ID"
+              placeholder="Gamertag/PSN ID/Steam ID/Origin ID"
               id="user"
               onChange={handleInput}
             />
           </div>
           <button onClick={getStats} type="button" className="stats__btn">
-            Search
+            <h2>Search</h2>
           </button>
         </form>
 
         <div className="stats__info">
+          <h1>{stats?.global?.name}</h1>
+          <h3>Level: {stats?.global?.level}</h3>
+          <br></br>
           BR RP: {stats?.global?.rank?.rankScore}
           <br></br>
-          BR Rank: {stats?.global?.rank?.rankName}
+          BR Rank: {stats?.global?.rank?.rankName}{" "}
+          {stats?.global?.rank?.rankDiv}
+          <br></br>
           <br></br>
           Arena RP: {stats?.global?.arena?.rankScore}
           <br></br>
           Arena Rank: {stats?.global?.arena?.rankName}
+          <br></br>
+          <br></br>
+          Lifetime BR Kills: {stats?.total?.kills?.value}
+          <br></br>
+          Lifetime BR Damage: {stats?.total?.damage?.value}
         </div>
       </div>
     </div>
