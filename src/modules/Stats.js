308094c6a0694e8aef1fba5dd5e2c64374619516
madhatter <matthew.rikard@gmail.com>
2022-05-25 10:07:18 -0400

    loading spinner added to Stats.js

--- src/modules/Stats.js
+++ src/modules/Stats.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./Stats.css";
 import axios from "../config/axios";
+import Spinner from "../components/Spinner";
 
 function Character() {
   const [platform, setPlatform] = useState("");
   const [user, setUser] = useState("");
   const [stats, setStats] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (event) => {
     setPlatform(event.target.value);
@@ -16,14 +18,37 @@ function Character() {
   };
 
   const getStats = async () => {
+    setIsLoading(true);
     const request = await axios.get(
       `bridge?auth=dN5M7fBTN7FCyZEWNlNj&player=${user}&platform=${platform}`
     );
     setStats(request.data);
+    setIsLoading(false);
     return request;
   };
 
   console.log(stats);
+
+  const renderStats = (
+    <>
+      <h1>{stats?.global?.name}</h1>
+      <h3>Level: {stats?.global?.level}</h3>
+      <br></br>
+      BR RP: {stats?.global?.rank?.rankScore}
+      <br></br>
+      BR Rank: {stats?.global?.rank?.rankName} {stats?.global?.rank?.rankDiv}
+      <br></br>
+      <br></br>
+      Arena RP: {stats?.global?.arena?.rankScore}
+      <br></br>
+      Arena Rank: {stats?.global?.arena?.rankName}
+      <br></br>
+      <br></br>
+      Lifetime BR Kills: {stats?.total?.kills?.value}
+      <br></br>
+      Lifetime BR Damage: {stats?.total?.damage?.value}
+    </>
+  );
 
   return (
     <div className="stats">
@@ -66,29 +91,18 @@ function Character() {
               onChange={handleInput}
             />
           </div>
-          <button onClick={getStats} type="button" className="stats__btn">
+          <button
+            onClick={getStats}
+            disabled={isLoading}
+            type="button"
+            className="stats__btn"
+          >
             <h2>Search</h2>
           </button>
         </form>
 
         <div className="stats__info">
-          <h1>{stats?.global?.name}</h1>
-          <h3>Level: {stats?.global?.level}</h3>
-          <br></br>
-          BR RP: {stats?.global?.rank?.rankScore}
-          <br></br>
-          BR Rank: {stats?.global?.rank?.rankName}{" "}
-          {stats?.global?.rank?.rankDiv}
-          <br></br>
-          <br></br>
-          Arena RP: {stats?.global?.arena?.rankScore}
-          <br></br>
-          Arena Rank: {stats?.global?.arena?.rankName}
-          <br></br>
-          <br></br>
-          Lifetime BR Kills: {stats?.total?.kills?.value}
-          <br></br>
-          Lifetime BR Damage: {stats?.total?.damage?.value}
+          {isLoading ? <Spinner /> : renderStats}
         </div>
       </div>
     </div>
